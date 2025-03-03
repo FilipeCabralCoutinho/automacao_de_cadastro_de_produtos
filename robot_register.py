3491918abdf3aebb7ffabc0f7fc6721d028b7edf
filipeCoutinho <filipeccoutinho2017@gmail.com>
2025-03-03 16:25:06 -0300

    first commit

---
+++ robot_register.py
@@ -0,0 +1,4 @@
+# Esse é um projeto de automação de uma tarefa repetitiva de cadastro de um produto.
+# O programava vai obter as insformações do arquivo em excel, acessará o site do formulário de cadastro,
+# vai inserir as informações e enviar o formulário.
+
