a1c17abba8e7b95851b7db384d687bef180c883b
filipeCoutinho <filipeccoutinho2017@gmail.com>
2025-03-03 17:15:19 -0300

    fim do projeto

--- robot_register.py
+++ robot_register.py
@@ -7,7 +7,7 @@ import pyautogui as auto
 import time
 
 # definir um tempo de intervalo padrão para cada etapa
-auto.PAUSE = 0.5
+auto.PAUSE = 0.8
 
 # obter arquivo com os dados
 table = pd.read_excel('Produtos.xlsx')
@@ -21,13 +21,38 @@ time.sleep(2)
 auto.press('enter')
 
 # laço para inserir os dados e enviar formulário
-for row in table:
-    cod_produto = table.loc['CodProduto']
-    # descricao = table.loc['Descrição']
-    # cod_marca = table.loc['CodMarca']
-    # preco = table.loc['Preço Unitário']
-    # categoria = table.loc['Categoria']
+for row in table.itertuples():
+    
+    cod_produto = row.CodProduto
+    descricao = row.descricao
+    cod_marca = row.CodMarca
+    preco = str(row.preco)
+    categoria = row.Categoria
+    
+    time.sleep(1)
+    auto.write(cod_produto)
 
-    auto.write()
+    auto.press('tab', presses=2)
+    auto.write(descricao)
+
+    auto.press('tab', presses=2)
+    auto.write(cod_marca)
+
+    auto.press('tab', presses=2)
+    auto.write(preco)
+
+    auto.press('tab', presses=2)
+    auto.write(categoria)
+    
+    auto.press('tab')
+    auto.press('enter')
+
+    time.sleep(2)
+    auto.press('tab', presses=16)
+    auto.press('enter')
+
+    time.sleep(2)
+    auto.press('tab', presses=4)
+    
     
 
