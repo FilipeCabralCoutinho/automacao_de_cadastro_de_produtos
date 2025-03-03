3fe6c2497df973319e4e1dc541f8802ae2caa9e9
filipeCoutinho <filipeccoutinho2017@gmail.com>
2025-03-03 16:43:53 -0300

    formulário aberto

--- robot_register.py
+++ robot_register.py
@@ -4,4 +4,30 @@
 
 import pandas as pd
 import pyautogui as auto
+import time
+
+# definir um tempo de intervalo padrão para cada etapa
+auto.PAUSE = 0.5
+
+# obter arquivo com os dados
+table = pd.read_excel('Produtos.xlsx')
+
+# abrir o formulário
+auto.press('win')
+auto.write('edge')
+auto.press('enter')
+auto.write('https://forms.office.com/r/yiDmntkg8v')
+time.sleep(2)
+auto.press('enter')
+
+# laço para inserir os dados e enviar formulário
+for row in table:
+    cod_produto = table.loc['CodProduto']
+    # descricao = table.loc['Descrição']
+    # cod_marca = table.loc['CodMarca']
+    # preco = table.loc['Preço Unitário']
+    # categoria = table.loc['Categoria']
+
+    auto.write()
+    
 
